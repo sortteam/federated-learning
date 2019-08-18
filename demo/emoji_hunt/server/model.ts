677655f0ea0e017db4afe6d2d886a284cf9a3fc0
graykode <nlkey2022@gmail.com>
2019-08-19 00:52:24 +0900

    edit FL server

--- demo/emoji_hunt/server/model.ts
+++ demo/emoji_hunt/server/model.ts
@@ -16,23 +16,15 @@
  */
 
 import * as tf from '@tensorflow/tfjs';
-import {loadFrozenModel} from '@tensorflow/tfjs-converter';
 import * as federated from 'federated-learning-server';
-
-// tslint:disable:max-line-length
-const MODEL_URL =
-    'https://storage.googleapis.com/learnjs-data/emoji_scavenger_hunt/web_model.pb';
-const WEIGHT_MANIFEST =
-    'https://storage.googleapis.com/learnjs-data/emoji_scavenger_hunt/weights_manifest.json';
-
 const LEARNING_RATE = 0.01;
 
 const MODEL_INPUT_WIDTH = 224;
-const NUM_LABELS = 424;
+const NUM_LABELS = 10;
 // Load the model & set it up for training
 export async function setupModel(saveDir: string):
     Promise<federated.FederatedServerModel> {
-  const model = await loadFrozenModel(MODEL_URL, WEIGHT_MANIFEST);
+  const model = await tf.loadGraphModel('https://ywj-horovod.s3.ap-northeast-2.amazonaws.com/model/model.json');
   const weights = model.weights;
 
   // TODO: there must be a better way
@@ -63,7 +55,7 @@ export async function setupModel(saveDir: string):
     return tf.losses.logLoss(label, preds) as tf.Scalar;
   };
 
-  const inputShape = [MODEL_INPUT_WIDTH, MODEL_INPUT_WIDTH, 3];
+  const inputShape = [MODEL_INPUT_WIDTH, MODEL_INPUT_WIDTH, 1];
   const outputShape = [NUM_LABELS];
   const varsAndLoss = new federated.FederatedServerDynamicModel(
       {vars, saveDir, predict, loss, optimizer, inputShape, outputShape});
